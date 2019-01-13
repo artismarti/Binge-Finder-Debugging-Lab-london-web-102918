0f11169c7c23f98e1aaa56b7e13ba1d713b3019e
artismarti <artismarti@gmail.com>
2019-01-13 20:11:22 +0000

    Done.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,48 +1,49 @@
-import React, { Component } from 'react';
-import Adapter from '../Adapter';
-import TVShowList from './TVShowList';
-import Nav from './Nav';
-import SelectedShowContainer from './SelectedShowContainer';
-import { Grid } from 'semantic-ui-react';
-
-
+import React, { Component } from 'react'
+import Adapter from '../Adapter'
+import TVShowList from './TVShowList'
+import Nav from './Nav'
+import SelectedShowContainer from './SelectedShowContainer'
+import { Grid } from 'semantic-ui-react'
 
 class App extends Component {
   state = {
     shows: [],
-    searchTerm: "",
-    selectedShow: "",
+    searchTerm: '',
+    selectedShow: '',
     episodes: [],
-    filterByRating: "",
+    filterByRating: '',
   }
 
   componentDidMount = () => {
-    Adapter.getShows().then(shows => this.setState({shows}))
+    Adapter.getShows().then(shows => this.setState({ shows }))
   }
 
   componentDidUpdate = () => {
     window.scrollTo(0, 0)
   }
 
-  handleSearch (e){
+  handleSearch = e => {
     this.setState({ searchTerm: e.target.value.toLowerCase() })
   }
 
-  handleFilter = (e) => {
-    e.target.value === "No Filter" ? this.setState({ filterRating:"" }) : this.setState({ filterRating: e.target.value})
+  handleFilter = e => {
+    e.target.value === 'No Filter'
+      ? this.setState({ filterByRating: '' })
+      : this.setState({ filterByRating: e.target.value })
   }
 
-  selectShow = (show) => {
-    Adapter.getShowEpisodes(show.id)
-    .then((episodes) => this.setState({
-      selectedShow: show,
-      episodes
-    }))
+  selectShow = show => {
+    Adapter.getShowEpisodes(show.id).then(episodes =>
+      this.setState({
+        selectedShow: show,
+        episodes: episodes,
+      })
+    )
   }
 
   displayShows = () => {
-    if (this.state.filterByRating){
-      return this.state.shows.filter((s)=> {
+    if (this.state.filterByRating) {
+      return this.state.shows.filter(s => {
         return s.rating.average >= this.state.filterByRating
       })
     } else {
@@ -50,21 +51,36 @@ class App extends Component {
     }
   }
 
-  render (){
+  render() {
     return (
       <div>
-        <Nav handleFilter={this.handleFilter} handleSearch={this.handleSearch} searchTerm={this.state.searchTerm}/>
+        <Nav
+          handleFilter={this.handleFilter}
+          handleSearch={this.handleSearch}
+          searchTerm={this.state.searchTerm}
+        />
         <Grid celled>
           <Grid.Column width={5}>
-            {!!this.state.selectedShow ? <SelectedShowContainer selectedShow={this.state.selectedShow} allEpisodes={this.state.episodes}/> : <div/>}
+            {!!this.state.selectedShow ? (
+              <SelectedShowContainer
+                selectedShow={this.state.selectedShow}
+                allEpisodes={this.state.episodes}
+              />
+            ) : (
+              <div />
+            )}
           </Grid.Column>
           <Grid.Column width={11}>
-            <TVShowList shows={this.displayShows()} selectShow={this.selectShow} searchTerm={this.state.searchTerm}/>
+            <TVShowList
+              shows={this.displayShows()}
+              selectShow={this.selectShow}
+              searchTerm={this.state.searchTerm}
+            />
           </Grid.Column>
         </Grid>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
